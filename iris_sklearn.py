9725d09dd214f7b844bd0c22ae57657671ef3922
Hai Ning <haining@microsoft.com>
2017-04-28 10:54:19 -0400

    log regularization rate

--- iris_sklearn.py
+++ iris_sklearn.py
@@ -1,9 +1,10 @@
 # Please make sure scikit-learn is included the conda_dependencies.yml file.
 
-from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
 import pickle
 import sys
+
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
 from azureml_sdk import data_collector
 
 run_logger = data_collector.current_run() 
@@ -18,14 +19,19 @@ print ('Iris dataset shape: {}'.format(iris.data.shape))
 # load features and labels
 X, Y = iris.data, iris.target
 
+# change regularization rate and you will likely get a different accuracy.
+reg = 0.01
+# log the regulizarion rate
+run_logger.metrics.custom_scalar("Regularization", reg)
+
 # train a logistic regression model
-# C is the inverse of the regularization strenght. Change this value to get a different accuracy.
-clf1 = LogisticRegression(C=100).fit(X, Y)
+clf1 = LogisticRegression(C=1/reg).fit(X, Y)
 print (clf1)
 
-# record accuracy
 accuracy = clf1.score(X, Y)
 print ("Accuracy is {}".format(accuracy))
+
+# log accuracy
 run_logger.metrics.custom_scalar("Accuracy", accuracy)
 
 # serialize the model on disk
@@ -44,4 +50,6 @@ X_new = [[3.0, 3.6, 1.3, 0.25]]
 print ('New sample: {}'.format(X_new))
 pred = clf2.predict(X_new)
 print ('Predicted class: {}'.format(pred))
+
+# log the predicted class
 run_logger.metrics.custom_scalar("Predicted Class", pred)
