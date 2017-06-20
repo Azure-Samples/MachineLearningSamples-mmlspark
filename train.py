5d90e0dc416ac30d2bd602e8a3c0b2e551fa35a3
hning86 <mit_hai@hotmail.com>
2017-06-19 21:49:55 -0400

    updated

--- train.py
+++ train.py
@@ -4,34 +4,49 @@ import pyspark
 import os
 import requests
 
+from pyspark.ml.classification import LogisticRegression
 import mmlspark
 from mmlspark.TrainClassifier import TrainClassifier
-from pyspark.ml.classification import LogisticRegression
+from mmlspark.ComputeModelStatistics import ComputeModelStatistics
 
+# Start Spark application
 spark = pyspark.sql.SparkSession.builder.appName("Adult Census Income").getOrCreate()
 
+# Download AdultCensusIncome.csv from Azure CDN. This file has 32,561 rows.
 dataFile = "AdultCensusIncome.csv"
 if not os.path.isfile(dataFile):
     r = requests.get("https://amldockerdatasets.azureedge.net/" + dataFile)
     with open(dataFile, 'wb') as f:    
         f.write(r.content)
 
+# Create a Spark dataframe out of the csv file.
 data = spark.createDataFrame(pd.read_csv(dataFile, dtype={" hours-per-week": np.float64}))
+# Choose a few relevant columns and the label column.
 data = data.select([" education", " marital-status", " hours-per-week", " income"])
 
+# Split data into train and test.
 train, test = data.randomSplit([0.75, 0.25], seed=123)
-model = TrainClassifier(model=LogisticRegression(), labelCol=" income", numFeatures=256).fit(train)
+
 print("********* TRAINING DATA ***********")
 print(train.limit(10).toPandas())
 
-from mmlspark.ComputeModelStatistics import ComputeModelStatistics
+# Use TrainClassifier in mmlspark to train a logistic regression model. Notice that we don't have to do any one-hot encoding, or vectorization. 
+# mmlspark does those tasks for us.
+model = TrainClassifier(model=LogisticRegression(), labelCol=" income", numFeatures=256).fit(train)
+
+# predict on the test dataset
 prediction = model.transform(test)
+
+# compute model metrics
 metrics = ComputeModelStatistics().transform(prediction)
+
 print("******** MODEL METRICS ************")
 print(metrics.limit(10).toPandas())
 
 print("******** SAVE THE MODEL ***********")
 model.write().overwrite().save(".outputs/AdultCensus.mml")
+
+# save model in wasb if running in HDI.
 #model.write().overwrite().save("wasb:///models/AdultCensus.mml")
 
 
